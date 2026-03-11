2a62d181be3f3bb41087fd00873646dbd08afe5d
Franco Votta <votta.franco16@gmail.com>
2026-03-10 22:53:59 -0300

    CRUD ready - day 2

--- src/server.js
+++ src/server.js
@@ -19,6 +19,17 @@ app.get("/products", (req, res) => {
     res.json(products);
 });
 
+app.get("/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const product = products.find((p) => p.id === id);
+
+    if (!product) {
+        return res.status(404).json({message : "Producto no encontrado"});
+    }
+
+    res.json(product);
+});
+
 app.post("/products", (req, res) => {
     const newProduct = {
         id: products.length + 1,
@@ -30,6 +41,33 @@ app.post("/products", (req, res) => {
     products.push(newProduct);
     res.status(201).json(newProduct);
 });
+
+app.put("/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const product = products.find((p) => p.id === id);
+
+    if (!product) {
+        return res.status(404).json({message : "Producto no encontrado"});
+    }
+
+    product.name = req.body.name ?? product.name;
+    product.price = req.body.price ?? product.price;
+    product.stock = req.body.stock ?? product.stock;
+
+    res.json(product);
+})
+
+app.delete("/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const productExists = products.some((p) => p.id === id);
+
+    if (!productExists) {
+        return res.status(404).json({message : "Producto no encontrado"});
+    }
+
+    products = products.filter((p) => p.id !== id);
+    res.json({message : "Producto eliminado correctamente"}) 
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
